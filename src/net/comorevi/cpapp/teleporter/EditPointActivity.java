214e6968ba8c98e4f18d19507adc75d9d47883c6
こもれび / popkechupki <hobbypopkechupki@gmail.com>
2019-01-27 11:45:31 +0900

    Initial commit

---
+++ src/net/comorevi/cpapp/teleporter/EditPointActivity.java
@@ -0,0 +1,23 @@
+package net.comorevi.cpapp.teleporter;
+
+import net.comorevi.cphone.cphone.application.ApplicationManifest;
+import net.comorevi.cphone.cphone.model.Bundle;
+import net.comorevi.cphone.cphone.model.Response;
+import net.comorevi.cphone.cphone.widget.activity.ReturnType;
+import net.comorevi.cphone.cphone.widget.activity.base.ListActivity;
+
+public class EditPointActivity extends ListActivity {
+    public EditPointActivity(ApplicationManifest manifest) {
+        super(manifest);
+    }
+
+    @Override
+    public void onCreate(Bundle bundle) {
+
+    }
+
+    @Override
+    public ReturnType onStop(Response response) {
+        return null;
+    }
+}
---
+++ src/net/comorevi/cpapp/teleporter/MainActivity.java
@@ -0,0 +1,56 @@
+package net.comorevi.cpapp.teleporter;
+
+import net.comorevi.cphone.cphone.CPhone;
+import net.comorevi.cphone.cphone.application.ApplicationManifest;
+import net.comorevi.cphone.cphone.model.Bundle;
+import net.comorevi.cphone.cphone.model.ListResponse;
+import net.comorevi.cphone.cphone.model.Response;
+import net.comorevi.cphone.cphone.widget.activity.ReturnType;
+import net.comorevi.cphone.cphone.widget.activity.base.ListActivity;
+import net.comorevi.cphone.cphone.widget.element.Button;
+
+public class MainActivity extends ListActivity {
+
+    private Bundle bundle;
+    private CPhone cPhone;
+
+    public MainActivity(ApplicationManifest manifest) {
+        super(manifest);
+    }
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        this.bundle = bundle;
+        this.cPhone = bundle.getCPhone();
+        this.setTitle(bundle.getString("title_home"));
+        this.setContent(bundle.getString("content_home"));
+
+        this.addButton(new Button().setText(bundle.getString("button_home1")));
+        this.addButton(new Button().setText(bundle.getString("button_home2")));
+        this.addButton(new Button().setText(bundle.getString("button_home3")));
+    }
+
+    @Override
+    public ReturnType onStop(Response response) {
+        ListResponse listResponse = (ListResponse) response;
+        switch (listResponse.getButtonIndex()) {
+            case 0:
+                new SelectLevelActivity(getManifest()).start(cPhone.getPlayer(), bundle.getStrings());
+                return ReturnType.TYPE_CONTINUE;
+            case 1:
+                new SelectPointActivity(getManifest()).start(cPhone.getPlayer(), bundle.getStrings());
+                return ReturnType.TYPE_CONTINUE;
+            case 2:
+                new SelectServerActivity(getManifest()).start(cPhone.getPlayer(), bundle.getStrings());
+                return ReturnType.TYPE_CONTINUE;
+            case 3:
+                new EditPointActivity(getManifest()).start(cPhone.getPlayer(), bundle.getStrings());
+                return ReturnType.TYPE_CONTINUE;
+            case 4:
+                new SettingsActivity(getManifest()).start(cPhone.getPlayer(), bundle.getStrings());
+                return ReturnType.TYPE_CONTINUE;
+        }
+
+        return ReturnType.TYPE_END;
+    }
+}
